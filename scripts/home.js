87294bd92973d75ec4b6f0d6d9ef5ff3aefae156
faran <faran.7j99@gmail.com>
2024-09-02 09:39:18 +0330

    Use data/products.js file

--- scripts/home.js
+++ scripts/home.js
@@ -1,55 +1,3 @@
-const products = [
-    {
-        image: './images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 45,
-            counts: 87
-        },
-        priceCents: 1090
-    },
-
-    {
-        image: './images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 40,
-            counts: 127
-        },
-        priceCents: 2095
-    },
-
-    {
-        image: './images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 45,
-            counts: 56
-        },
-        priceCents: 799
-    },
-
-    {
-        image: './images/products/black-2-slot-toaster.jpg',
-        name: '2 Slot Toaster - Black',
-        rating: {
-            stars: 50,
-            counts: 2197
-        },
-        priceCents: 1899
-    },
-
-    {
-        image: './images/products/6-piece-white-dinner-plate-set.jpg',
-        name: '6 Piece White Dinner Plate Set',
-        rating: {
-            stars: 40,
-            counts: 37
-        },
-        priceCents: 2067
-    }
-];
-
 generateDataStructureHTML();
 
 function generateDataStructureHTML(){
@@ -67,8 +15,8 @@ function generateDataStructureHTML(){
                     ${product.name}
                 </div>
                 <div class="product-rating-c">
-                    <img class="product-rating-star" src="./images/ratings/rating-${product.rating.stars}.png" alt="">
-                    <div class="product-rating-num">${product.rating.counts}</div>
+                    <img class="product-rating-star" src="./images/ratings/rating-${product.rating.stars * 10}.png" alt="">
+                    <div class="product-rating-num">${product.rating.count}</div>
                 </div>
                 <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
                 <div class="product-quantity">
