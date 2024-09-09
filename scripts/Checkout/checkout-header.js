5ff1f95804a73f5288c9a3f00df01533bc34f8ad
faran <faran.7j99@gmail.com>
2024-09-09 08:11:33 +0330

    move cheackout header to its own file and use MVC to make it

---
+++ scripts/Checkout/checkout-header.js
@@ -0,0 +1,26 @@
+import { getCartTotalQuantity } from "../../data/cart.js";
+
+export function renderCheckoutHeader() {
+    generateCheckoutHeaderHTML();
+}
+
+function generateCheckoutHeaderHTML() {
+    const headerHTML = document.querySelector('.checkout-header');
+    
+    headerHTML.innerHTML = 
+    `            
+        <div class="left-section">
+            <a href="./home.html">
+                <img class="amazon-logo" src="./images/amazon-logo.png" alt="">
+                <img class="amazon-logo-sm" src="./images/amazon-mobile-logo.png" alt="">
+            </a>
+        </div>
+        <div class="middle-section">
+            Checkout (<a class="items-quantity-link" href="./home.html">
+            <span class="js-total-products-number">${getCartTotalQuantity()}</span> items</a>)
+        </div>
+        <div class="right-section">
+            <img src="./images/icons/checkout-lock-icon.png" alt="">
+        </div>
+    `;
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import { renderOrderSummary } from "./Checkout/order-summary.js";
 import { renderPaymentSummary } from "./Checkout/payment-summary.js";
+import { renderCheckoutHeader } from "./Checkout/checkout-header.js";
 
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
