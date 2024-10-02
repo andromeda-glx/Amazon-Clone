8d6264121ef7224a781f8bba49e61669b8045a76
faran <faran.7j99@gmail.com>
2024-10-02 16:29:57 +0330

    add search by keywords functionality

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 import { cart } from './data/cart.js';
-import { products, loadProducts } from './data/products.js';
+import { products, loadProducts, filterProductsByName, filterProductsByKeywords } from './data/products.js';
 import { updateCartQuantity } from "./utils/update-cart-quantity.js";
 import { generateDefaultHeader } from "./utils/amazon-default-header.js";
 
@@ -19,7 +19,7 @@ function generateDataStructureHTML() {
 
     if(searchParam){
         filteredList = products.filter(product => {
-            return product.name.toLowerCase().includes(searchParam.toLowerCase());
+            return product.name.toLowerCase().includes(searchParam.toLowerCase()) || product.keywords.includes(searchParam.toLowerCase());
         });
     }
 
